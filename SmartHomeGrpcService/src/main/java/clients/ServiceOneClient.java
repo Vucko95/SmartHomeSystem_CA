75662fccbe49a161a5b11ff013ecb6ec3fe61959
Ronald Suplina <ronal@host.docker.internal>
2022-03-06 17:11:55 +0000

    Client 1st RPC

--- SmartHomeGrpcService/src/main/java/clients/ServiceOneClient.java
+++ SmartHomeGrpcService/src/main/java/clients/ServiceOneClient.java
@@ -1,5 +1,8 @@
 package clients;
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 import gymRoomControlService.TempRequest;
 import gymRoomControlService.TempResponse;
 import gymRoomControlService.gymRoomControlServiceGrpc;
@@ -8,20 +11,28 @@ import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
 public class ServiceOneClient {
-	public static void main(String[] args) {
+	// creating a message
+	public static TempRequest inputRequestDataScanner(Scanner scanner) {
+		int number = scanner.nextInt();
+		TempRequest request = TempRequest.newBuilder().setTempInput(number).build();
+		return request;
+		
+	}
+	public static void main(String[] args) throws InterruptedException {
 	// create channel
 	int port = 50051;
 	String host = "localhost";
-	
 	ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();	
-		
+		// stub creation
+		// specific to service
 	gymRoomControlServiceGrpc.gymRoomControlServiceStub stub = gymRoomControlServiceGrpc.newStub(channel);
 	
+	//     / we call RPC
 	StreamObserver<TempRequest> requestObserver = stub.changeRoomTemp(new StreamObserver<TempResponse>()
 			{
 		@Override
 		public void onNext(TempResponse response) {	
-			System.out.println("Test");
+			System.out.println(response.getTempOutput());
 			}
 
 		@Override
@@ -36,9 +47,15 @@ public class ServiceOneClient {
 		
 		});
 	
-	
-	
-	
+		Scanner scanner = new Scanner(System.in);
+		TempRequest request = inputRequestDataScanner(scanner);
+//		System.out.println("Input another ");
+//		String answer = scanner.next();
+		System.out.println("Thank you for inputing temperature");
+//		System.out.println("Temeprature set to" + TempOutput);
+		scanner.close();
+		requestObserver.onCompleted();
+		channel.shutdownNow().awaitTermination(3, TimeUnit.SECONDS);	
 	
 	}//main
 }//class
