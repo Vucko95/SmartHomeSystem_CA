aec020e384bceb1c1a0a3dc347205156a4ab439d
Ronald Suplina <tyger995@gmail.com>
2022-04-03 19:59:01 +0100

    GUI Graphic Part Only

--- SmartHomeGrpcService/src/main/java/gui/WorkoutTrackerGUI.java
+++ SmartHomeGrpcService/src/main/java/gui/WorkoutTrackerGUI.java
@@ -2,19 +2,28 @@ package gui;
 
 
 
+import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
 	import io.grpc.ManagedChannel;
 	import io.grpc.ManagedChannelBuilder;
 
 	public class WorkoutTrackerGUI   implements ActionListener {
-
+		private JComboBox<String> request1;
+		private JLabel response1;
+		private JTextArea responseAll;
 		private ManagedChannel manageService3;
 		public static void main(String[] args) {
 			WorkoutTrackerGUI gui = new WorkoutTrackerGUI();
@@ -43,15 +52,61 @@ import javax.swing.border.EmptyBorder;
 
 			panel.setBorder(new EmptyBorder(new Insets(100, 100, 100, 100)));
 
-//			panel.add(());
-//			panel.add(());
+			panel.add(WorkoutTrackerPanel1());
+			panel.add(WorkoutTrackerPanel2());
 			frame.setSize(500, 500);
 			frame.add(panel);
 			frame.pack();
 			frame.setVisible(true);	
 		}
 		
-		
+		private JPanel WorkoutTrackerPanel1() {
+			JPanel panel = new JPanel();
+			BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.X_AXIS);
+			JLabel label = new JLabel("Workout");
+			panel.add(label);
+			panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+			request1 = new JComboBox<>();
+			request1.setModel(
+					new javax.swing.DefaultComboBoxModel<>(new String[] { "Walking", "Running", "Stairclimb", "Cycling" }));
+			panel.add(request1);
+			panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+			JButton button = new JButton("Check");
+			button.addActionListener(this);
+			panel.add(button);
+			panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+			response1 = new JLabel("--");
+			panel.add(response1);
+			panel.setLayout(boxlayout);
+
+			return panel;
+
+		}
+		private JPanel WorkoutTrackerPanel2() {
+			JPanel panel = new JPanel();
+			BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.X_AXIS);
+			JLabel label = new JLabel("");
+			panel.add(label);
+			panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+			JButton button = new JButton("View All");
+			button.addActionListener(this);
+			panel.add(button);
+			panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+			responseAll = new JTextArea();
+			
+			panel.setLayout(boxlayout);
+			responseAll = new JTextArea (6, 10);
+			responseAll .setLineWrap(true);
+			responseAll .setWrapStyleWord(true);
+			panel.add(responseAll);
+			return panel;
+
+		}
 
 
 		public void actionPerformed(ActionEvent e) { }
